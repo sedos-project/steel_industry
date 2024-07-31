4965f60d1934c60ac3a1b4169388f43b3ea34365
FelixMau <fmau@posteo.de>
2024-07-31 12:44:54 +0200

    Precommit and adjust helper_sink_exo_steel

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ https://packaging.python.org/guides/distributing-packages-using-setuptools/
 https://github.com/pypa/sampleproject
 """
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
 from os import path
+
+# Always prefer setuptools over distutils
+from setuptools import find_packages, setup
 
 here = path.abspath(path.dirname(__file__))
 
