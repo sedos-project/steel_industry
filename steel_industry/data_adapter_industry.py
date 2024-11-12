b74b2a9ecf6bce7f2637716466d69beb5fced8c4
SabineHaas <sabine.haas@rl-institut.de>
2024-11-12 16:21:39 +0100

    Add energy system dump

--- steel_industry/data_adapter_industry.py
+++ steel_industry/data_adapter_industry.py
@@ -41,86 +41,97 @@ EnergySystem.from_datapackage = classmethod(deserialize_energy_system)
 Model.add_constraints_from_datapackage = deserialize_constraints
 
 DEBUG = True  # set to False for full run. DEBUG reduces to 5 time steps per period
+READ_DUMP = False
 
-
-download_collection(
-    "https://databus.openenergyplatform.org/felixmaur/collections/steel_industry_test/"
-)
-
-logger.info("Reading Structure\n")
-structure = Structure(
-    "SEDOS_Modellstruktur",
-    process_sheet="Processes_O1",
-    parameter_sheet="Parameter_Input-Output",
-    helper_sheet="Helper_O1",
-)
-
-adapter = Adapter(
-    "steel_industry_test",
-    structure=structure,
-)
-
-logger.info("Building Adapter Map\n")
-
-# create dictionary with all found in- and outputs
-processes = pd.read_excel(io=structure.structure_file, sheet_name="Processes_O1", usecols=("process", "facade adapter (oemof)"), index_col="process")
-helper_processes = pd.read_excel(io=structure.structure_file, sheet_name="Helper_O1", usecols=("process", "facade adapter (oemof)"), index_col="process")
-process_adapter_map = pd.concat(
-    [processes, helper_processes]
-        ).to_dict(orient="dict")["facade adapter (oemof)"]
-
-logger.info("Building datapackage...\n")
-dp = DataPackage.build_datapackage(
-    adapter=adapter,
-    process_adapter_map=process_adapter_map,
-    parameter_map=PARAMETER_MAP_STEEL,
-    debug=DEBUG,  # set DEBUG to False for full run. DEBUG reduces to 5 time steps per period
-    bus_map=load_yaml(Path(__file__).parent / "mappings" / "BUS_MAP.yaml"),
-)
-datapackage_path = pathlib.Path(__file__).parent / "datapackage"
-
-# delete datapackage before saving it as otherwise old elements are kept
-shutil.rmtree(datapackage_path)
-
-dp.save_datapackage_to_csv(str(datapackage_path))
-
-
-logger.info("Building EnergySystem\n")
-es = EnergySystem.from_datapackage(
-    path="datapackage/datapackage.json",
-    typemap={
-        "bus": Bus,
-        "excess": Excess,
-        "commodity": Commodity,
-        "conversion": Conversion,
-        "load": Load,
-        "volatile": Volatile,
-        "mimo": MIMO,
-        "storage": Storage,
-        "conversion_ghg": ConversionGHG,
-        "commodity_ghg": CommodityGHG,
-    },
-)
-
-logger.info("Building Model...\n")
-m = Model(es)
-logger.info("Solving Model...\n")
-m.solve(solver="cbc")
-
-termination_condition = m.solver_results["Solver"][0]["Termination condition"]
-if m.solver_results["Solver"][0]["Termination condition"] == "infeasible":
-    logger.warning(f"termination condition is '{termination_condition}'\n")
+if DEBUG:
+    es_dump_path = pathlib.Path(__file__).parent / "results" / "energysystem"
 else:
-    logging.info(f"Problem solved. (termination condition '{termination_condition}')\n")
+    es_dump_path = pathlib.Path(__file__).parent / "results" / "energysystem_full"
 
-logger.info("Processing Results")
-es.results = postprocessing.get_results(m)
-es.params = postprocessing.get_inputs(m)
-# dump energy system to read results again
-es.dump(es_dump_path)
+if not READ_DUMP:
+    # delete collection before downloading
+    # shutil.rmtree(pathlib.Path(__file__).parent / "collections" / "steel_industry_test")
+    download_collection(
+        "https://databus.openenergyplatform.org/felixmaur/collections/steel_industry_test/"
+    )
+
+    logger.info("Reading Structure\n")
+    structure = Structure(
+        "SEDOS_Modellstruktur",
+        process_sheet="Processes_O1",
+        parameter_sheet="Parameter_Input-Output",
+        helper_sheet="Helper_O1",
+        # new_emission_constraint_inputs=False,  # todo delete !!!
+    )
+
+    adapter = Adapter(
+        "steel_industry_test",
+        structure=structure,
+    )
+
+    logger.info("Building Adapter Map\n")
+
+    # create dictionary with all found in- and outputs
+    processes = pd.read_excel(io=structure.structure_file, sheet_name="Processes_O1", usecols=("process", "facade adapter (oemof)"), index_col="process")
+    helper_processes = pd.read_excel(io=structure.structure_file, sheet_name="Helper_O1", usecols=("process", "facade adapter (oemof)"), index_col="process")
+    process_adapter_map = pd.concat(
+        [processes, helper_processes]
+            ).to_dict(orient="dict")["facade adapter (oemof)"]
+
+    logger.info("Building datapackage...\n")
+    dp = DataPackage.build_datapackage(
+        adapter=adapter,
+        process_adapter_map=process_adapter_map,
+        parameter_map=PARAMETER_MAP_STEEL,
+        debug=DEBUG,  # set DEBUG to False for full run. DEBUG reduces to 5 time steps per period
+        bus_map=load_yaml(Path(__file__).parent / "mappings" / "BUS_MAP.yaml"),
+    )
+    datapackage_path = pathlib.Path(__file__).parent / "datapackage"
+
+    # delete datapackage before saving it as otherwise old elements are kept
+    shutil.rmtree(datapackage_path)
+
+    dp.save_datapackage_to_csv(str(datapackage_path))
+
+
+    logger.info("Building EnergySystem\n")
+    es = EnergySystem.from_datapackage(
+        path="datapackage/datapackage.json",
+        typemap={
+            "bus": Bus,
+            "excess": Excess,
+            "commodity": Commodity,
+            "conversion": Conversion,
+            "load": Load,
+            "volatile": Volatile,
+            "mimo": MIMO,
+            "storage": Storage,
+            "conversion_ghg": ConversionGHG,
+            "commodity_ghg": CommodityGHG,
+            "co2_emission_limit": CO2EmissionLimit,
+        },
+    )
+
+    logger.info("Building Model...\n")
+    m = Model(es)
+    logger.info("Solving Model...\n")
+    m.solve(solver="cbc")
+
+    termination_condition = m.solver_results["Solver"][0]["Termination condition"]
+    if m.solver_results["Solver"][0]["Termination condition"] == "infeasible":
+        logger.warning(f"termination condition is '{termination_condition}'\n")
+    else:
+        logging.info(f"Problem solved. (termination condition '{termination_condition}')\n")
+
+    logger.info("Processing Results")
+    es.results = postprocessing.get_results(m)
+    es.params = postprocessing.get_inputs(m)
+    # dump energy system to read results again
+    es.dump(es_dump_path)
 else:
-es = EnergySystem()
-es.restore(es_dump_path)
+    es = EnergySystem()
+    es.restore(es_dump_path)
+
 # process and save results
 file_name = pathlib.Path(__file__).parent / "results" / "test" / "results.csv"
 postprocessing.process_results(es, file_name)
