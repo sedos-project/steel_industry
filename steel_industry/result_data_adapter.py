9e98dda9fd33e34d0979c0f631b70c00bd01638e
SabineHaas <sabine.haas@rl-institut.de>
2024-11-20 12:29:33 +0100

    Read results from repository directory and write dashboard results to csv

--- steel_industry/result_data_adapter.py
+++ steel_industry/result_data_adapter.py
@@ -1,6 +1,3 @@
-
-from openpyxl import load_workbook
-from openpyxl.utils import get_column_letter
 import pandas as pd
 from functions import name_function, var_name_function
 import pathlib
@@ -18,7 +15,7 @@ output = pd.DataFrame(columns= columns)
 
 # Preparation Input
 
-data = pd.read_csv("results/test/results_invested_capacity.csv",
+data = pd.read_csv(pathlib.Path(__file__).parent / "results" / "test" / "results.csv",
                         sep=",")
 
 # Applies a string replacement to each value in the name column.
@@ -51,23 +48,24 @@ output.scenario = "o_steel_tokio"
 # save data as csv
 
 # save data as excel sheet
-
-with pd.ExcelWriter("results/dashboard_results/SEDOS_output.xlsx") as writer:
-    output.to_excel(writer, sheet_name="SEDOS_output", index=False)
-
-# Lade die erstellte Excel-Datei mit openpyxl
-wb = load_workbook("results/dashboard_results/SEDOS_output.xlsx")
-
-# Funktion zum Anpassen der Spaltenbreite
-def adjust_column_width(sheet):
-    for col_index, column_cells in enumerate(sheet.columns, start=1):
-        # Berechne die maximale Länge der Werte in der Spalte
-        max_length = max(len(str(cell.value) or "") for cell in column_cells)  # Handle None values
-        column_letter = get_column_letter(col_index)
-        # Setze die Spaltenbreite basierend auf der maximalen Länge der Zelleninhalte
-        sheet.column_dimensions[column_letter].width = max_length + 5
-
-    # Passe die Spaltenbreite an
-sheet = wb["SEDOS_output"]
-adjust_column_width(sheet)
-
+output.to_csv(pathlib.Path(__file__).parent / "results" / "dashboard_results" / "o_steel_tokio_results.csv",
+                        sep=";")
+# with pd.ExcelWriter("results/dashboard_results/SEDOS_output.xlsx") as writer:
+#     output.to_excel(writer, sheet_name="SEDOS_output", index=False)
+#
+# # Lade die erstellte Excel-Datei mit openpyxl
+# wb = load_workbook("results/dashboard_results/SEDOS_output.xlsx")
+#
+# # Funktion zum Anpassen der Spaltenbreite
+# def adjust_column_width(sheet):
+#     for col_index, column_cells in enumerate(sheet.columns, start=1):
+#         # Berechne die maximale Länge der Werte in der Spalte
+#         max_length = max(len(str(cell.value) or "") for cell in column_cells)  # Handle None values
+#         column_letter = get_column_letter(col_index)
+#         # Setze die Spaltenbreite basierend auf der maximalen Länge der Zelleninhalte
+#         sheet.column_dimensions[column_letter].width = max_length + 5
+#
+#     # Passe die Spaltenbreite an
+# sheet = wb["SEDOS_output"]
+# adjust_column_width(sheet)
+#
