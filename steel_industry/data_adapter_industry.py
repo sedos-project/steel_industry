6e28682d633f807ff94af991d8e728f7f1c38396
SabineHaas <sabine.haas@rl-institut.de>
2024-12-10 14:20:23 +0100

    Set DEBUG to False

--- steel_industry/data_adapter_industry.py
+++ steel_industry/data_adapter_industry.py
@@ -40,7 +40,8 @@ EnergySystem.from_datapackage = classmethod(deserialize_energy_system)
 
 Model.add_constraints_from_datapackage = deserialize_constraints
 
-DEBUG = True  # set to False for full run. DEBUG reduces to 5 time steps per period
+# settings
+DEBUG = False  # set to False for full run. DEBUG reduces to 24 time steps per period
 READ_DUMP = False  # set to True to process previous results
 UNITS = [  # for unit conversion of data_adapter
     "MtCO2eq",
