310936f8b3e46583653d6b39f49bf371d2f234b4
SabineHaas <sabine.haas@rl-institut.de>
2024-12-12 16:36:14 +0100

    Add emi_co2_f_ind

--- steel_industry/functions.py
+++ steel_industry/functions.py
@@ -219,7 +219,7 @@ def helper_results(helper,output):
 
 def calculate_co2_eq(sedos_results):
     # filter sedos_results by processes with ch4 and n20 emissions
-    emis = ["emi_ch4_f_ind", "emi_n2o_f_ind", "emi_co2_p_ind", "emi_co2_neg_imp", "emi_co2_reusable", "emi_co2_f_pow"]
+    emis = ["emi_ch4_f_ind", "emi_n2o_f_ind", "emi_co2_p_ind", "emi_co2_neg_imp", "emi_co2_reusable", "emi_co2_f_pow", "emi_co2_f_ind"]
     sedos_emis = sedos_results[sedos_results["output_groups"].isin(emis)]
 
     # Pivot to facilitate calculations
@@ -232,7 +232,7 @@ def calculate_co2_eq(sedos_results):
     # fill NaN with "0"
     pivot_df = pivot_df.fillna(0)
     pivot_df["emi_co2_eq"] = (pivot_df["emi_co2_p_ind"] + pivot_df["emi_co2_neg_imp"] + pivot_df["emi_co2_reusable"] +
-                              pivot_df["emi_co2_f_pow"] + 28 * pivot_df["emi_ch4_f_ind"] + 265 * pivot_df["emi_n2o_f_ind"])
+                              pivot_df["emi_co2_f_pow"] + pivot_df["emi_co2_f_ind"] + 28 * pivot_df["emi_ch4_f_ind"] + 265 * pivot_df["emi_n2o_f_ind"])
 
     # create dataframe with new rows included calculated co2 - emissions
     new_rows = pivot_df[["process", "year", "emi_co2_eq"]].rename(columns={"emi_co2_eq": "value"})
